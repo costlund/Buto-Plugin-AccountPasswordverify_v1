06ae53be6ab74006cc103f0e83a425a77f3bc931
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-09-27 10:35:19 +0200

    Add params to Javascript.

--- js/function.js
+++ js/function.js
@@ -1,7 +1,8 @@
 function PluginAccountPasswordverify_v1(){
-  this.data = {method: null}
-  this.verify = function(method){
+  this.data = {method: null, params: {}}
+  this.verify = function(method, params){
     this.data.method = method;
+    this.data.params = params;
     PluginWfBootstrapjs.modal({id: 'modal_passwordverify', url:'passwordverify/verify', lable:PluginI18nJson_v1.i18n('Verify'), size:'sm'});
   }
   this.verify_capture = function(result){
@@ -14,7 +15,8 @@ function PluginAccountPasswordverify_v1(){
   }
   this.run_method = function(){
     var method = PluginAccountPasswordverify_v1.data.method;
-    method();
+    var params = PluginAccountPasswordverify_v1.data.params;
+    method(params);
   }
 }
 var PluginAccountPasswordverify_v1 = new PluginAccountPasswordverify_v1();
